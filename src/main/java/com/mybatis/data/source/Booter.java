9e2aaa18f78f668a5cb3c017f070b75afbedfb4b
yin.huang <923430998@qq.com>
2018-03-26 13:41:42 +0800

    1

--- src/main/java/com/mybatis/data/source/Booter.java
+++ src/main/java/com/mybatis/data/source/Booter.java
@@ -8,13 +8,12 @@ import com.mybatis.data.source.dao.ITestDAO;
 public class Booter {
 
 	public static void main(String[] args) {
-			String[] sources = new String[] { "spring/applicationContext.xml",
-					"spring/applicationContext-persistence.xml", "spring/applicationContext-dao.xml",
-					"spring/applicationContext-service.xml" };
-			ApplicationContext ctx = new ClassPathXmlApplicationContext(sources);
-			ITestDAO testDAO = (ITestDAO) ctx.getBean("testDAO");
-			System.out.println(testDAO.getBscInfoEntityListByLac("11"));
-			System.out.println("bsc info Server Started... ");
+		String[] sources = new String[] { "spring/applicationContext.xml", "spring/applicationContext-persistence.xml",
+				"spring/applicationContext-dao.xml", "spring/applicationContext-service.xml" };
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(sources);
+		ITestDAO testDAO = (ITestDAO) ctx.getBean("testDAO");
+		System.out.println(testDAO.getInfoEntityList("11"));
+		System.out.println("bsc info Server Started... ");
 	}
 
 }
